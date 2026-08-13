3e139e536c93e83b997573a4be56d3f69c306c25
Chaitanya Manohar Satpute <chaitanyasatpute729@gmail.com>
2026-08-13 15:02:56 +0530

    Time: 117 ms (5.01%), Space: 48 MB (20.63%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0)
+            {
+                count++;
+            }
+        }
+
+        for(int i=0;i<count;i++)
+        {
+            for(int j=0;j<nums.length-1-i;j++)
+            {
+               if(nums[j]==0)
+               {
+                     int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+               }
+            }
+        }
+
+    }
+}
