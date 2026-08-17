1bb172656d01499f55118c65e6d828267e77a0d0
Chaitanya Manohar Satpute <chaitanyasatpute729@gmail.com>
2026-08-17 09:51:48 +0530

    Time: 3 ms (69.81%), Space: 49.1 MB (50.23%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-
-        int max=-1;
+        int m=-1;
         for(int i=arr.length-1;i>=0;i--)
         {
             int temp=arr[i];
-            arr[i]=max;
-            max=Math.max(max,temp);
+            arr[i]=m;
+            m=Math.max(m,temp);
         }
         return arr;
-
         
     }
 }
