1c7bf52aa180c88c3dc2f8826f9e6c236a25a6ac
Chaitanya Manohar Satpute <chaitanyasatpute729@gmail.com>
2026-09-01 18:57:20 +0530

    Time: 4 ms (99.07%), Space: 47.7 MB (68.89%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,27 +1,31 @@
 class Solution {
 
-    public boolean palindrome(int i,int j,String s)
+    public boolean palin(String s,int i,int j)
     {
-         while(i<j)
-         {
-            if(s.charAt(i)!=s.charAt(j))
+        while(i<j)
+        {
+             if(s.charAt(i)!=s.charAt(j))
             {
                 return false;
             }
-            i++;
-            j--;
-         }
-         return true;
+            else{
+                i++;
+                j--;
+            }
+
+        }
+        return true;
     }
+
     public boolean validPalindrome(String s) {
-        
+
         int i=0;
         int j=s.length()-1;
         while(i<j)
         {
             if(s.charAt(i)!=s.charAt(j))
             {
-                return palindrome(i+1,j,s) || palindrome(i,j-1,s);
+                return palin(s,i+1,j) || palin(s,i,j-1);
 
             }
             else{
@@ -29,8 +33,10 @@ class Solution {
                 j--;
             }
         }
+
         return true;
 
 
+        
     }
 }
