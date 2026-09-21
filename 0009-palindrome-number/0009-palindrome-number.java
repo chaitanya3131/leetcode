761f16e6dab13424f65d36c7586fa81c4c734991
Chaitanya Manohar Satpute <chaitanyasatpute729@gmail.com>
2026-09-21 14:28:26 +0530

    Time: 5 ms (83.47%), Space: 46.1 MB (33.36%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,24 +1,26 @@
 class Solution {
     public boolean isPalindrome(int x) {
+
         int n=x;
         int rev=0;
         while(n!=0)
         {
             int k=n%10;
-            rev=rev*10+k;
+            rev=(rev*10)+k;
             n=n/10;
         }
-        if(x<0)
+        if(rev<0)
         {
             return false;
         }
-        else if(rev==x)
+        if(rev==x)
         {
             return true;
         }
-        else {
+        else{
             return false;
         }
+
         
     }
 }
